2c53af9ab6f3706b16066898fe82cf1d17cca481
Rubyst_42 <exwarvlad@gmail.com>
2018-10-30 22:23:38 +0200

    change route give_top_posts to GET

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resource :posts, only: [:create] do
     post :create_mark
-    post :give_top_posts
+    get :give_top_posts
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-%x(curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d ' {"post": {"header":"x-fa-fafds","body":"yoyoy4242", "ip":"127.0.0.1", "user_login": "ttt424242"}}'  http://localhost:3000/posts)
-%x(curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d ' {"post": {"num": 5, "post_id": 1055}}'  http://localhost:3000/posts/create_mark)
-%x(curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d ' {"post": {"n": 5}}'  http://localhost:3000/posts/give_top_posts)
+%x(curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d ' {"post": {"header":"x-fa-fafds","body":"yoyoy42", "ip":"127.0.0.1", "user_login": "ttt424242"}}'  http://localhost:3000/posts)
+%x(curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d ' {"post": {"num": 3, "post_id": 1055}}'  http://localhost:3000/posts/create_mark)
+%x(curl -v -H "Accept: application/json" -H "Content-type: application/json" -X GET -d ' {"post": {"n": 5}}'  http://localhost:3000/posts/give_top_posts)
